734367a85996440eb8b0c00f45b88f37a6df7d45
Mildoverlag <peter.thimas@gmail.com>
2026-01-07 18:55:48 -0500

    v2

--- MicroController/MicroController.ino
+++ MicroController/MicroController.ino
@@ -1,12 +1,12 @@
 // consts
 
   //ID of Micro Controler used to diffrentite when mutiple are used 
-  const byte ID = 0x02;
+  const byte ID = 0x01;
   const byte START = 0x0f;
   const byte END = 0xf0;
   
   // insert data pins of where positve lead of button is can support max of 255 buttons
-  const int BUTTONS[] = {53,51};
+  const int BUTTONS[] = {53,51,49};
   const int BUTTONSCOUNT = sizeof(BUTTONS) / sizeof(BUTTONS[0]);
 
 void setup() {
